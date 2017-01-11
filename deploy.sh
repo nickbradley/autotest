53b61db8f939c30e5cf1895561aefc08fcb8dac8
Nick Bradley <nickbradley@live.ca>
2017-01-10 21:39:46 -0800

    support config file

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,12 @@ redis
 
 # Create CouchDB container
 docker create \
-  --name autotest-db \
+  --name autotest-db1 \
   --publish 11312:5984 \
   --env COUCHDB_USER="${DB_ADMIN_USERNAME}" \
   --env COUCHDB_PASSWORD="${DB_ADMIN_PASSWORD}" \
-  --volume /home/nick/Projects/autotest/dbdata:/usr/local/var/lib/couchdb \
+  --volume /home/nickbradley/autotest/dbdata:/usr/local/var/lib/couchdb \
+  --volume /home/nickbradley/autotest/dbconfig:/usr/local/etc/couchdb/local.d \
 couchdb
 
 
