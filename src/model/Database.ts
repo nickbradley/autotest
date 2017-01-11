2743c8348a8f445a61daff099be10af1b203a8fc
Nick Bradley <nickbradley@live.ca>
2017-01-10 19:11:24 -0800

    test

--- src/model/Database.ts
+++ src/model/Database.ts
@@ -117,18 +117,14 @@ export class Connection {
   async authenticate(): Promise<AuthenticationResponse> {
     let that = this;
     return new Promise<AuthenticationResponse>((fulfill, reject) => {
-      console.log('Calling auth with ', that.username, that.password);
-
       that.dbServer.auth(that.username, that.password, (err, body, headers) => {
         console.log(body, headers);
         if (err) {
-          console.log('error', err, headers)
           reject(err);
         }
 
         if (headers && headers['set-cookie']) {
           that.auth = headers['set-cookie'][0];
-          console.log('cookie-set');
         }
         that.dbServer = require('nano')({url: that.address, cookie: that.auth});
         fulfill(body);
