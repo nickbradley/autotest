1a08aac58bdb853432a3ba7256440d3c0dd8d78b
Nick Bradley <nickbradley@live.ca>
2017-02-19 07:06:26 -0800

    Add better support for redis

---
+++ src/controller/RedisManager.ts
@@ -0,0 +1,39 @@
+import RedisClient from "../model/RedisClient";
+
+/**
+ * Provides access to the redis client via the singleton.
+ */
+export default class RedisManager {
+  public get client(): RedisClient {
+    return RedisSingleton.getInstance().client;
+  }
+}
+
+/**
+ * Singleton to ensure there is only one connection to Redis.
+ */
+class RedisSingleton {
+  private static _instance: RedisSingleton;
+  private _client: RedisClient;
+
+
+  private constructor() {
+    this._client = new RedisClient(6389);
+  }
+
+
+  public static getInstance(): RedisSingleton {
+    if (!RedisSingleton._instance)
+      RedisSingleton._instance = new RedisSingleton();
+
+    return RedisSingleton._instance;
+  }
+
+
+  /**
+   * Provides access to the single instance of the redis client.
+   */
+  public get client(): RedisClient {
+    return this._client;
+  }
+}
