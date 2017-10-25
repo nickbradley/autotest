663ec08292f37099084000ca6e37066085b73759
Andrew Stec <steca@cs.ubc.ca>
2017-10-25 11:53:56 -0700

    Removed old references to CouchDB

--- deploy.sh
+++ deploy.sh
@@ -27,15 +27,3 @@ do
 
 done
 
-# Create CouchDB container
-docker create \
-  --name autotest-db \
-  --publish 11312:5984 \
-  --env COUCHDB_USER="${DB_ADMIN_USERNAME}" \
-  --env COUCHDB_PASSWORD="${DB_ADMIN_PASSWORD}" \
-  --volume "${scriptDir}/.data/dbdata":/usr/local/var/lib/couchdb \
-  --volume "${scriptDir}/.data/dbconfig":/usr/local/etc/couchdb/local.d \
-couchdb
-
-
-./dbconfig.sh
